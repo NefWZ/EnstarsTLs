cd9f381b70262b5df5c358ab65e2b89ae3c3cdb7
Nef <nefariwolfj@gmail.com>
2025-12-25 22:10:01 -0500

    Update and rename character.js to characters.js

---
+++ characters.js
@@ -0,0 +1,28 @@
+const characterData = {
+  EichiTenshouin: {
+    name: 'Eichi',                     // Display name
+    unit: 'fine',                       // Their unit (optional)
+    img: 'https://github.com/NefWZ/img/blob/main/headshots/eichi.png?raw=true',  // Profile image
+    thumbnail: 'https://github.com/NefWZ/img/blob/main/thumbnails/eichi.png?raw=true', // Optional smaller icon
+     hue: 49.86,        // Hue Angle
+    color: '#FFF3B8' // Optional custom color
+  },
+
+ WataruHibiki: {
+    name: 'Wataru',                     // Display name
+    unit: 'fine',                       // Their unit (optional)
+    img: 'https://github.com/NefWZ/img/blob/main/headshots/wataru.png?raw=true',  // Profile image
+    thumbnail: 'https://github.com/NefWZ/img/blob/main/thumbnails/wataru.png?raw=true', // Optional smaller icon
+     hue: 49.86,        // Hue Angle
+    color: '#A1D8E2' // Optional custom color
+  },
+
+  char3: {
+    name: 'Character 3',
+    unit: 'Star Knights',
+    img: 'https://via.placeholder.com/50',
+    thumbnail: 'https://via.placeholder.com/30'
+  }
+
+  // Add more characters here...
+};
